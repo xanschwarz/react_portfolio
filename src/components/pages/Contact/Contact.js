72f0e5da0ed99dffbde0d0220b65e00ec22daf7f
Xander <xanschwarz@gmail.com>
2021-11-19 20:45:54 -0800

    Updated contact page, has some functionality and styling. Updated package.json for gh-pages.

--- src/components/pages/Contact/Contact.js
+++ src/components/pages/Contact/Contact.js
@@ -6,28 +6,99 @@ Entering text into the email address field prompts a notification if the user en
 
 -----------------------------------------------------------------------------------------------------------------------  */
 
-import React from 'react';
+import React, { useState } from 'react';
+import { validateEmail } from '../../../utils/helpers';
+import './Contact.css';
 
 export default function Contact() {
+  const [inputState, setInputState] = useState({
+    name: '',
+    email: '',
+    subject: '',
+    message: '',
+  });
+
+  const [errorState, setErrorState] = useState('');
+  const { name, email, subject, message } = inputState;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!errorState) {
+      console.log('Form submitted', inputState);
+    }
+  };
+
+  const handleChange = (e) => {
+    if (e.target.name === 'email') {
+      if (!validateEmail(e.target.value)) {
+        setErrorState('Please enter a valid email address');
+      } else {
+        setErrorState('');
+      }
+    } else {
+      if (e.target.value === '') {
+        setErrorState(`The ${e.target.name} field is required.`);
+      } else {
+        setErrorState('');
+      }
+    }
+    if (!errorState) {
+      setInputState({ ...inputState, [e.target.name]: e.target.value });
+      console.log('From input', inputState);
+    }
+  };
+
   return (
     <div>
       <h1>Contact Page</h1>
-      <p>
-        This is where my contact information will go with fields to contact me.
-      </p>
-      <p>
-        Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-        molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-        magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-        efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-        mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-        posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-        faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-        ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-        dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-        conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-        rhoncus. Etiam vel condimentum magna, quis tempor nulla.
-      </p>
+      <div className="container">
+        <form onSubmit={handleSubmit}>
+          <label for="name">Your Name</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            className="textInput"
+            placeholder="Please enter your name here."
+            onBlur={handleChange}
+          ></input>
+
+          <label for="email">Email Address</label>
+          <input
+            type="text"
+            id="email"
+            name="email"
+            className="textInput"
+            placeholder="Please enter your email address."
+            onBlur={handleChange}
+          ></input>
+
+          <label for="subject">Email subject</label>
+          <input
+            type="text"
+            id="subject"
+            name="subject"
+            className="textInput"
+            placeholder="Please enter an email subject here."
+            onBlur={handleChange}
+          ></input>
+
+          <label for="message">Email message</label>
+          <textarea
+            id="message"
+            name="message"
+            placeholder="Enter your email message here."
+            style={{ height: '200px' }}
+            onBlur={handleChange}
+          ></textarea>
+          {errorState && (
+            <div>
+              <p id="errorDisplay">{errorState}</p>
+            </div>
+          )}
+          <input type="submit" value="Submit" id="submitBtn"></input>
+        </form>
+      </div>
     </div>
   );
 }
